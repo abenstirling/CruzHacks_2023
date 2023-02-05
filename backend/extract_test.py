cb0af77ed06e8dae53f1b982824d50df4550eca2
abenstirling <abenstirling@pm.me>
2023-02-05 00:11:21 -0800

    Changes by ari

--- backend/extract_test.py
+++ backend/extract_test.py
@@ -7,19 +7,20 @@ openai.api_key = "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"
 
 
 def parse_menu(url):
-    #Grabbing link for recipe
+    # Grabbing link for recipe
     scraper = scrape_me(url)
 
-    #First part of prompt
-    pre_question = "\n\nI am a recipe ingredient scraping bot. My purpose is to convert natural-language ingredients list into formatted JSON output. Given the following input: \n"
-
-    #List refernce
-    reference = ['2 large eggs', 'Pinch fine sea salt', '1/2 Tbsp unsalted butter', '1/4 cup shredded mozzarella (low moisture, part-skim)']
-    ref_formatted = "\n".join([f'\'{thing}\',' for thing in reference])
+    # First part of prompt
+    pre_question = "\n\nI am a recipe ingredient scraping bot. My purpose is to convert natural-language ingredients " \
+                   "list into formatted JSON output. Given the following input: \n"
+    # List reference
+    reference = ['2 large eggs', 'Pinch fine sea salt', '1/2 Tbsp unsalted butter', '3 sweet potatoes',
+                 '1/4 cup shredded mozzarella (low moisture, part-skim)', '6 chives']
+    ref_formatted = "\n".join([f' \'{thing}\',' for thing in reference])
     print("FORMATTED JSON --> \n" + ref_formatted)
-    scraper_formatted = "\n".join([f'\'{thing}\',' for thing in scraper.ingredients()])
+    scraper_formatted = "\n".join([f' \'{thing}\',' for thing in scraper.ingredients()])
 
-    #Opening json reference
+    # Opening json reference
     with open("./extract_correct.json") as file:
         extract_correct = json.load(file)
         print("CORRECT JSON --> \n" + json.dumps(extract_correct))
@@ -31,11 +32,11 @@ def parse_menu(url):
                f"{new_output} ```\n{scraper_formatted}\n```"
     print(question)
 
-    #Asking question
-    response = openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0, max_tokens=100)
+    # Asking question
+    response = openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0, max_tokens=3000)
+    # Printing question
+    print(response["choices"][0]["text"])
+    return response["choices"][0]["text"]
 
-    #Printing question
-    print(response)
-    return(response)
 
 parse_menu('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
