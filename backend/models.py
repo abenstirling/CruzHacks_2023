4bc118d8207ce08daf85f72bea1cf0ab753fbd59
alanm319 <alanm319@gmail.com>
2023-02-04 18:59:22 -0800

    auth stuff

---
+++ backend/models.py
@@ -0,0 +1,44 @@
+
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class UserModel(BaseModel):
+    userName: str = Field(...)
+    password: str = Field(...)
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: str}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backend/test_main.py
+++ backend/test_main.py
@@ -4,22 +4,3 @@ from main import app
 
 client = TestClient(app)
 
-
-def test_create_item():
-    response = client.post(
-        "/",
-        headers={"X-Token": "coneofsilence"},
-        json={
-          "name": "Jane Doe",
-          "email": "jdoe@example.com",
-          "course": "Experiments, Science, and Fashion in Nanophotonics",
-          "gpa": "3.0"
-        },
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "name": "Jane Doe",
-        "email": "jdoe@example.com",
-        "course": "Experiments, Science, and Fashion in Nanophotonics",
-        "gpa": "3.0"
-    }
